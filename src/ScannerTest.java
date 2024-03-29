9000b93d6ff02b6aa90cd7d1dcb66a14ebee88cd
gabrieldbsousa <gabrieldbsousa@gmail.com>
2024-03-29 10:14:29 -0300

    input data I

--- src/ScannerTest.java
+++ src/ScannerTest.java
@@ -2,20 +2,23 @@ import java.util.Scanner;
 
 public class ScannerTest {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
-        String x;
-        x = sc.next();
-        System.out.println("Você digitou: " + x);
+        int x;
+        String s1, s2, s3;
 
-        int y;
-        y = sc.nextInt();
-        System.out.println("Você digitou o número: " + y);
+        x = sc.nextInt();
+        sc.nextLine();
+        s1 = sc.nextLine();
+        s2 = sc.nextLine();
+        s3 = sc.nextLine();
 
-        double z;
-        z = sc.nextDouble();
-        System.out.println("Você digitou o número: " + z);
-
+        System.out.println("DADOS DIGITADOS:");
+        System.out.println(x);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
         sc.close();
     }
 }
